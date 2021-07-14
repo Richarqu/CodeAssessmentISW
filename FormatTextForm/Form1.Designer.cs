92a9196e97414ebfd5b9261aee053ece4757379c
richa <richa@DESKTOP-BNB1VBL>
2021-07-14 14:15:52 +0100

    Add project files.

---
+++ FormatTextForm/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace FormatTextForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.txtOutput);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(38, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(552, 541);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(429, 512);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(110, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(9, 272);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(530, 239);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "See Result Below";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(9, 40);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(530, 208);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your text here";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 582);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ FormatTextForm/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormatTextForm
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * Below depicts the algorithm for print an output based on advised conditions
+         * Create a Windows Forms application that contain two Textboxes for both input text and output text
+         * Receive text input from the input Textbox
+         * Check the text received to ensure it has just 300 characters else remove the trailing characters
+         * Split the words in the remaining 300 characters and put them in a list
+         * Loop through each word and determine the following:
+         *      1. If the characters in each word are 3, change the word to capital letters
+         *      2. If the word represents the last word in the loop, reverse the word
+         * Add the words back to form the text after each loop
+         * Print out the final result in the output text
+         * Create a try - catch for each method and  print out any exception noticed inthe output text for review.
+        */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtOutput.Text = "";
+                if (!string.IsNullOrEmpty(txtInput.Text))
+                {
+                    //Remove other characters greater than 300
+                    string txtManip = DoTextManipulation(txtInput.Text);
+                    txtOutput.Text = string.IsNullOrEmpty(txtManip) ? "Empty Response" : txtManip;
+                }
+                else
+                {
+                    txtOutput.Text = "Kindly enter a text in the input box.";
+                }
+            }
+            catch (Exception ex)
+            {
+                //output error
+                txtOutput.Text = ex.Message;
+            }
+        }
+        public string DoTextManipulation(string input)
+        {
+            string output = string.Empty;
+            try
+            {
+                if (input.Length > 300)
+                {
+                    //do remove
+                    input = input.Remove(300, input.Length - 300);
+                    //split to words and do necessary checks
+                    output = ComputeOutput(input);
+                }
+                else
+                {
+                    output = ComputeOutput(input);
+                }
+            }
+            catch (Exception ex)
+            {
+                //output error
+                txtOutput.Text = ex.Message;
+            }
+            return output;
+        }
+        private string ComputeOutput(string input)
+        {
+            string formOutput = string.Empty;
+            try
+            {
+                //do spliting
+                List<string> inputWordsLen = input.Split(' ').ToList();
+                //count words
+                int wordCount = inputWordsLen.Count;
+                int i = 0;
+                foreach (string word in inputWordsLen)
+                {
+                    string theWord = string.Empty;
+                    i++;
+                    if (word.Length == 3)
+                    {
+                        //convert word with length = 3 to capital letters
+                        theWord = word.ToUpper();
+                    }
+                    else { theWord = word; }
+                    //reverse the last word and replace it with word
+                    theWord = i == wordCount - 1 ? ReverseString(word) : theWord;
+                    formOutput += theWord + " ";
+                }
+            }
+            catch(Exception ex)
+            {
+                //output error
+                txtOutput.Text = ex.Message;
+            }
+            return formOutput;
+        }
+        private string ReverseString(string input)
+        {
+            //convert each letter to char and put in char array for reversing
+            char[] inputChar = input.ToCharArray();
+            Array.Reverse(inputChar);
+            return new string(inputChar);
+        }
+
+        private void txtInput_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+        }
+    }
+}
